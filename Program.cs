458aa7b4a428ca63a6fa73f362890e2ef1695c80
Iris <39096994+RiisDev@users.noreply.github.com>
2025-02-25 06:35:39 -0700

    Update Program.cs | Note, add to gitignore

--- Program.cs
+++ Program.cs
@@ -3,20 +3,6 @@ using System.Text.Json;
 using Script.Bypasses;
 using Script.Util;
 
-Debug.WriteLine("\tFUCK\tYEET");
-
-string data = MethodEnhance.JsonSerialize(new
-{
-    Fuck = "dsfsdf       dsfsd   sdfsd",
-    Yeet = new {
-        fuck = 123,
-        dotnet = '2'
-    },
-    Pi = Math.PI
-});
-
-Debug.WriteLine(data);
-
 while (true) Console.ReadLine();
 
 //riisdevencryptaes
