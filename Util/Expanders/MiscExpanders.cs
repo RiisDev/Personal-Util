d0f4ffd4abdd714e700229c693735b10e703321a
Iris <39096994+RiisDev@users.noreply.github.com>
2025-03-02 04:03:39 -0700

    List print

--- Util/Expanders/MiscExpanders.cs
+++ Util/Expanders/MiscExpanders.cs
@@ -1,4 +1,6 @@
-﻿namespace Script.Util.Expanders
+﻿using System.Diagnostics;
+
+namespace Script.Util.Expanders
 {
     public static class MiscExpanders
     {
@@ -10,5 +12,14 @@
         }
 
         public static void Flip(ref this bool value) => value = !value;
+
+        public static void Print<T>(this IEnumerable<T> collection, bool console = false)
+        {
+            foreach (T item in collection)
+            {
+                Debug.WriteLine(item);
+                if (console) Console.WriteLine(item);
+            }
+        }
     }
 }
